6bea927bd06ff99e1d2686379daee5c26bfeae13
Ryan Ong <ryan.ong@downergroup.com>
2025-10-31 16:20:08 +1100

    disable tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,14 @@
-import pytest
+# import pytest
 
 
-def pytest_addoption(parser):
-    parser.addoption(
-        "--use-keyvault-secrets",
-        help='Get secrets from a keyvault instead of the environment.',
-        action='store_true', default=False
-)
+# def pytest_addoption(parser):
+#     parser.addoption(
+#         "--use-keyvault-secrets",
+#         help='Get secrets from a keyvault instead of the environment.',
+#         action='store_true', default=False
+# )
 
 
-@pytest.fixture(scope="session")
-def use_keyvault_secrets(request) -> str:
-    return request.config.getoption("use_keyvault_secrets")
+# @pytest.fixture(scope="session")
+# def use_keyvault_secrets(request) -> str:
+#     return request.config.getoption("use_keyvault_secrets")
